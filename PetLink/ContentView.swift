85130d346c4f7b91bc8048f60b291e78fcc34826
soldansd <danielsolovyev@gmail.com>
2024-08-19 17:43:08 +0300

    Initial Commit

---
+++ PetLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetLink
+//
+//  Created by Даниил Соловьев on 19/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetLink/PetLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetLinkApp.swift
+//  PetLink
+//
+//  Created by Даниил Соловьев on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PetLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
